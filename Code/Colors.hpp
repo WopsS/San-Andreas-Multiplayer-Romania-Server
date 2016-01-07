660b02270cfe5036406ae178ee86ea7f8bf9190d
Claudiu Widualk <claudiu_sharingen@yahoo.com>
2016-01-07 13:54:38 +0200

    Added more colors.

--- Code/Colors.hpp
+++ Code/Colors.hpp
@@ -3,6 +3,13 @@
 enum Color
 {
 	kBlack = 0x000000FF,
+	kBlue = 0x002AB8FF,
+	kBrown = 0x411D06FF,
+	kDarkBlue = 0x0D3487FF,
 	kFren = 0x5CAD5CFF, // Usually used for error message.
+	kGreen = 0x15B90EFF,
+	kGuardsmanRed = 0xB80A00FF,
+	kPurple = 0x670EB9FF,
+	kYellow = 0xEAEB28F,
 	kWhite = 0xFFFFFFFF
 };
